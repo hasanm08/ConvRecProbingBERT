c5de5dfcd39ee4554c80e955bfdd537a59fda1a6
Amirhassan Amirmahani <39486543+hasanm08@users.noreply.github.com>
2023-06-28 20:06:46 +0330

    Add files via upload

---
+++ data/create_recommendation_datasets.sh
@@ -0,0 +1,18 @@
+# which python
+# python make_seq_rec_data.py \
+#   --task music \
+#   --ratings_path ./search/CDs_and_Vinyl.json \
+#   --item_names_path ./search/meta_CDs_and_Vinyl.json \
+#   --output_path ./recommendation/music
+
+python make_seq_rec_data.py \
+  --task gr \
+  --ratings_path ./recommendation/goodreads_interactions_dedup.json \
+  --item_names_path ./recommendation/books_names.csv \
+  --output_path ./recommendation/gr
+
+# python make_seq_rec_data.py \
+#   --task ml25m \
+#   --ratings_path ./recommendation/ml25m_ratings.csv \
+#   --item_names_path ./recommendation/movies_names.csv \
+#   --output_path ./recommendation/ml25m
